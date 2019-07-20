88c71e51af9af107c21451a444a8165186ee1a17
Eduard Panasiuk <ed.belarus@gmail.com>
2019-07-20 16:39:36 +0300

    Add dependency

--- SPM-client/ViewController.swift
+++ SPM-client/ViewController.swift
@@ -7,14 +7,27 @@
 //
 
 import UIKit
+import library
 
 class ViewController: UIViewController {
 
+    private let label = UILabel(frame: .zero)
+    private let generator = Generator()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.textColor = .black
+        label.text = generator.generate()
+        
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        label.widthAnchor.constraint(lessThanOrEqualTo: view.widthAnchor).isActive = true
+        label.heightAnchor.constraint(greaterThanOrEqualToConstant: 0).isActive = true
+        
     }
-
-
 }
 
