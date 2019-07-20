549fc66554a635678f669749ce191ea49a93a984
Eduard Panasiuk <ed.belarus@gmail.com>
2019-07-20 12:34:49 +0300

    Add client project

---
+++ client/SPM-client/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SPM-client
+//
+//  Created by Eduard Panasiuk on 7/20/19.
+//  Copyright © 2019 Eduard Panasiuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
